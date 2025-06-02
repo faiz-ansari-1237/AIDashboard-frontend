18b689049891a48db1ce6c2e6562b491130f4beb
Faiz Ansari <135289427+faiz-ansari-1237@users.noreply.github.com>
2025-06-02 21:13:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react'
 
 // Import individual components
 import Header from './components/Header';
